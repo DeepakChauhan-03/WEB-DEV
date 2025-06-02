4d42de67e6b9259eb6c4f67f5e93ae9f57ac6fdb
Deepak Chauhan <deepakchauhan6141@gmail.com>
2025-06-03 00:22:23 +0530

    Random number

---
+++ Raghav js/randomNumber.js
@@ -0,0 +1,7 @@
+//WAP to generate a random number between 0 and 9
+// let num = Math.random()*10;
+// console.log(num);
+
+//WAP to generate a random integer between 0 and 9
+let num = Math.floor(Math.random()*10);
+console.log(num);
