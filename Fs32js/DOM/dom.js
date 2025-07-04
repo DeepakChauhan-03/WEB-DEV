6bcac60cf2b8b550efbd449e4cf8d2b6a8251297
Deepak Chauhan <deepakchauhan6141@gmail.com>
2025-07-04 20:43:17 +0530

    Games of sqaure Done

--- Fs32js/DOM/dom.js
+++ Fs32js/DOM/dom.js
@@ -1,3 +1,5 @@
 // document.getElementsByTagName() 
 // document.getElementsByClassName()
 // document.querySelector()
+
+//webgapge ke context me -- Kuch hone ko event kehte hai 
--- Raghav js/Basic dom/GameofSquare/script.js
+++ Raghav js/Basic dom/GameofSquare/script.js
@@ -1,6 +1,9 @@
 let s1 = document.getElementById("sq1");
 s1.addEventListener("mouseenter",function(){
-    s1.style.background = "red";
+     let r1 = Math.floor(Math.random()*256);
+    let r2 = Math.floor(Math.random()*256);
+    let r3 = Math.floor(Math.random()*256);
+    s1.style.background = `rgb(${r1},${r2},${r3})`;
     let r = Math.floor(Math.random()*100);
     s1.innerHTML = `<h1>${r}</h1>` ;
 })
@@ -10,10 +13,46 @@ s1.addEventListener("mouseleave",function(){
 })
 
 
-
-
-
-
 let s2 = document.getElementById("sq2");
+s2.addEventListener("mouseenter",function(){
+    let r1 = Math.floor(Math.random()*256);
+    let r2 = Math.floor(Math.random()*256);
+    let r3 = Math.floor(Math.random()*256);
+    s2.style.background = `rgb(${r1},${r2},${r3})`;
+    let r = Math.floor(Math.random()*100);
+    s2.innerHTML = `<h1>${r}</h1>` ;
+})
+s2.addEventListener("mouseleave",function(){
+    s2.style.background = "white";
+    s2.innerHTML = "<h1>2</h1>";
+})
+
+
 let s3 = document.getElementById("sq3");
+s3.addEventListener("mouseenter",function(){
+     let r1 = Math.floor(Math.random()*256);
+    let r2 = Math.floor(Math.random()*256);
+    let r3 = Math.floor(Math.random()*256);
+    s3.style.background = `rgb(${r1},${r2},${r3})`;
+    let r = Math.floor(Math.random()*100);
+    s3.innerHTML = `<h1>${r}</h1>` ;
+})
+s3.addEventListener("mouseleave",function(){
+    s3.style.background = "white";
+    s3.innerHTML = "<h1>3</h1>";
+})
+
+
 let s4 = document.getElementById("sq4");
+s4.addEventListener("mouseenter",function(){
+     let r1 = Math.floor(Math.random()*256);
+    let r2 = Math.floor(Math.random()*256);
+    let r3 = Math.floor(Math.random()*256);
+    s4.style.background = `rgb(${r1},${r2},${r3})`;
+    let r = Math.floor(Math.random()*100);
+    s4.innerHTML = `<h1>${r}</h1>` ;
+})
+s4.addEventListener("mouseleave",function(){
+    s4.style.background = "white";
+    s4.innerHTML = "<h1>4</h1>";
+})
